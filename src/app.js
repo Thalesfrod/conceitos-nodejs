c72a8f9adcc3b7ba0462bbf8641cb12f505de64a
Thalesfrod <thalesfrod@gmail.com>
2020-08-21 13:21:02 -0300

    Objective complete.
    Added 3 more routes, now the user is able to modify, delete and like an repository.

--- src/app.js
+++ src/app.js
@@ -11,34 +11,80 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
+  //Rota que lista todos os repositórios;
   const { title } = request.query;
     
   const results = title
-   ? repositories.filter(project => project.title.includes(title))
+   ? repositories.filter(project => repo.title.includes(title))
    : repositories;
  
   return response.json(results);
 });
 
 app.post("/repositories", (request, response) => {
+  //rota que cria repositorios
   const {title, url, techs} = request.body;
-  const project = {id: uuidv4(), title, url, techs, likes: 0};
+  const repo = {id: uuidv4(), title, url, techs, likes: 0};
 
-  repositories.push(project);
+  repositories.push(repo);
 
-  return response.json(project)
+  return response.json(repo)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  // A rota deve alterar apenas o title, a url e as techs
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({"error": 'Repository not found.'})
+  }
+  
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repoIndex].likes, 
+  }
+
+  repositories[repoIndex] = repo;
+
+  return response.json(repo);
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  // A rota deve deletar o repositório com o id presente nos parâmetros da rota
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({"error": 'Repository not found.'})
+  };
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  //A rota deve aumentar o número de likes do repositório específico escolhido
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json({"error": 'Repository not found.'})
+  };
+
+repositories[repoIndex].likes++;
+
+return response.json(repositories[repoIndex]);
+
 });
 
 module.exports = app;
